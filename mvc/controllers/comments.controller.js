80ea8660cf49f8c40cfec4d9cf33958dec8c695d
TianYu-00 <imniyeye@gmail.com>
2024-05-31 17:15:34 +0100

    complete task 18 - ADVANCED: PATCH /api/comments/:comment_id

--- mvc/controllers/comments.controller.js
+++ mvc/controllers/comments.controller.js
@@ -1,9 +1,19 @@
-const { removeCommentById } = require("../models/comments.model");
+const { removeCommentById, updateCommentById } = require("../models/comments.model");
 exports.deleteCommentById = (request, response, next) => {
   const commentId = request.params.comment_id;
   removeCommentById(commentId)
     .then(() => {
       response.status(204).send();
+    })
+    .catch(next);
+};
+
+exports.patchCommentById = (request, response, next) => {
+  const commentId = request.params.comment_id;
+  const body = request.body;
+  updateCommentById(commentId, body)
+    .then((updatedComment) => {
+      response.status(200).send(updatedComment);
     })
     .catch(next);
 };
--- mvc/models/comments.model.js
+++ mvc/models/comments.model.js
@@ -7,8 +7,19 @@ exports.removeCommentById = async (commentId) => {
   if (commentCount.rows.length === 0) {
     return Promise.reject({ status: 404, msg: errorMsg });
   }
+  return db.query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [commentId]);
+};
 
-  return db.query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [commentId]).then((result) => {
-    // console.log(result.rows);
-  });
+exports.updateCommentById = async (commentId, body) => {
+  const comment = await db.query(`SELECT votes FROM comments WHERE comment_id = $1`, [commentId]);
+  const query = `UPDATE comments
+    SET votes = $1
+    WHERE comment_id = $2 RETURNING *;`;
+  const errorMsg = "comment does not exist";
+
+  if (comment.rows.length === 0) {
+    return Promise.reject({ status: 404, msg: errorMsg });
+  }
+  const newVoteValue = comment.rows[0].votes + body.inc_votes;
+  return db.query(query, [newVoteValue, commentId]).then(({ rows }) => rows[0]);
 };
