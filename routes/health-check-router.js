2144ab055a4ddcbe1c37b374ee6bbf3633d933de
Tian <66271788+TianYu-00@users.noreply.github.com>
2025-06-15 15:47:51 +0100

    Update health-check-router.js

--- routes/health-check-router.js
+++ routes/health-check-router.js
@@ -4,7 +4,8 @@ const healthCheckRouter = express.Router();
 
 healthCheckRouter.get("/", async (req, res) => {
   try {
-    await db.query(`SELECT 1;`);
+    // await db.query(`SELECT 1;`);
+    await db.query(`SELECT * FROM users LIMIT 1;`);
     res.status(200).json({ success: true, message: "Database is awake and healthy.", data: null });
   } catch (error) {
     res.status(500).json({ success: false, message: "Database unreachable.", data: null });
